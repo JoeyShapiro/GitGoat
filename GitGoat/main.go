2d3254d2e4e9a25c934708e3098cd2e4fe50fdde
JoeyShapiro <joeyashapiro@gmail.com>
2024-03-30 20:39:04 -0500

    looks like i have another weekend

--- GitGoat/main.go
+++ GitGoat/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// log to file instead of stdout
+	f, _ := os.OpenFile("gin.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0660)
+	gin.DefaultWriter = f
+
 	router := gin.Default()
 	router.GET("webhook/events", webhook)
 
