08d2831d9e1d5baef56fd83d920e0404a19bf71a
JoeyShapiro <joeyashapiro@gmail.com>
2024-04-06 08:30:01 -0500

    updated build

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+mkdir bin
+
 cd GitGoat
-go build -o git-goat
+go build -o ../bin/git-goat
+GOOS=linux GOARCH=amd64 go build -o ../bin/git-goat-linux-amd64
+GOOS=linux GOARCH=arm64 go build -o ../bin/git-goat-linux-arm64
 cd ..
 
 cd goat-pusher
